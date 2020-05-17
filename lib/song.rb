d9509a78d12e6b144a783490d1b7eaa82e8ef96c
Aaron Schuyler <asky28@gmail.com>
2020-05-17 00:24:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song 
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :genre
+  attr_reader :artist
   @@count = 0
   @@artists = []
   def initialize
     @@count +=1
   end
-  
+  def artist=(artist)
+    @artist = artist
+    @@artists << artist
+  end
 end
